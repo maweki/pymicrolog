4835612e1c81141bb96d81b6a7988ae0f2fece76
Mario Wenzel <maweki@gmail.com>
2023-12-20 16:31:42 +0100

    more prototyping

--- example.py
+++ example.py
@@ -3,6 +3,9 @@ import operator
 import time
 
 a = relation("a")
+b = relation("b")
+c = relation("c")
+d = relation("d")
 X = variable("X")
 Y, Z = variables(*"YZ")
 mylt = oracle(operator.lt)
@@ -13,8 +16,14 @@ rules = [
   a(2)@start,
   a(7)@start,
   a(12),
+  b(1,2),
   a(0)@next,
   a(X)@next <= a(X) & ~mylt(5, X) & mylt(X, 5),
+  b(X, X) <= a(X),
+  b(X, X) <= b(X, ...),
+  b(X, X) <= b(..., X),
+  c() <= ~b(..., ...),
+  d() <= a(...) & b(X, X),
   tcall1()@next <= a(2),
   tcall2()@next <= ~a(7),
 ]
--- pymicrolog.py
+++ pymicrolog.py
@@ -3,6 +3,12 @@ from enum import Enum
 class TemporalAnnotation(Enum):
   start = 1
   next = 2
+
+  def __repr__(self):
+      if self == start:
+          return "start"
+      if self == next:
+          return "next"
 start = TemporalAnnotation.start
 next = TemporalAnnotation.next
 
@@ -39,6 +45,9 @@ class Conjunction(object):
   def asConjunction(self): # very bad name
     return list(self.literals)
 
+  def __repr__(self):
+      return " & ".join(repr(l) for l in self.literals)
+
   def __and__(self, other):
     return Conjunction(*self.asConjunction(), *other.asConjunction())
 
@@ -50,6 +59,9 @@ class Variable(object):
 
   def __init__(self, varname):
     self.varname = varname
+
+  def __repr__(self):
+      return f"Var({self.varname})"
 
 class Relation(object):
   relname = None
@@ -69,6 +81,9 @@ class Relation(object):
   def __le__(self):
     raise ValueError("Missing ()?")
 
+  def __repr__(self):
+      return f"R({self.relname})"
+
 class Formula(object):
   fn = None
   args = None
@@ -76,11 +91,18 @@ class Formula(object):
     self.fn = fn
     self.args = args
 
+  def variables(self):
+      return frozenset(arg for arg in self.args if isinstance(arg, Variable))
+
   def asRule(self):
     return Rule(head=self)
 
   def asConjunction(self):
     return [self]
+
+  def __repr__(self):
+    return f"{self.fn}{repr(self.args)}"
+
 
   def __and__(self, other):
     return Conjunction(*self.asConjunction(), *other.asConjunction())
@@ -104,8 +126,31 @@ class Rule(object):
     self.head = head
     self.body = body
 
+  def __repr__(self):
+      if self.body is None:
+          return f"{repr(self.head)}."
+      else:
+          return f"{repr(self.head)} <- {repr(self.body)}."
+
   def asRule(self):
     return self
+
+  def isRangeRestricted(self):
+      if self.body is None:
+          return not bool(self.head.variables())
+      positives = set()
+      dependents= set(self.head.variables())
+      for lit in self.body.asConjunction():
+          if isinstance(lit, Formula):
+              positives.update(lit.variables())
+          else:
+              dependents.update(lit.variables())
+      return positives.issuperset(dependents)
+
+  def variables(self):
+      if self.body is None:
+          return self.head.variables()
+      return self.head.variables() | self.body.variables()
 
 class TempAnnotatedFormula(object):
   fn = None
@@ -122,12 +167,21 @@ class TempAnnotatedFormula(object):
   def __le__(self, other):
     return Rule(self, other)
 
+  def variables(self):
+      return Formula(self.fn, self.args).variables()
+
+  def __repr__(self):
+      return f"{self.fn}{repr(self.args)}@{repr(self.temporalAnnotation)}"
+
 class CallFormula(object):
   fn = None
   args = None
   def __init__(self, oracle, args):
     self.fn = oracle.fn
     self.args = args
+
+  def variables(self):
+      return frozenset(arg for arg in self.args if isinstance(arg, Variable))
 
   def __matmul__(self, other):
     if other is not next:
@@ -143,6 +197,15 @@ class NegatedFormula(object):
   def __init__(self, orig):
     self.orig = orig
 
+  def asConjunction(self):
+      return [self]
+
+  def variables(self):
+      return self.orig.variables()
+
+  def __repr__(self):
+      return f"~{repr(self.orig)}"
+
 class NegatedOracleFormula(object):
   orig = None
   def __init__(self, orig):
@@ -153,6 +216,9 @@ class NegatedOracleFormula(object):
 
   def __and__(self, other):
     return Conjunction(*self.asConjunction(), *other.asConjunction())
+
+  def variables(self):
+      return self.orig.variables()
 
 class OracleFormula(object):
   fn = None
@@ -170,24 +236,35 @@ class OracleFormula(object):
   def __invert__(self):
     return NegatedOracleFormula(self)
 
+  def variables(self):
+      return frozenset(arg for arg in self.args if isinstance(arg, Variable))
+
 class Program(object):
   rules = None
   strata = None
   initial = None
+  always = None
   next = None
   def __init__(self, rules, name=None, fnmapping=dict(), reorderBodies=True):
     self.fnmapping = fnmapping
     rules = list(rule.asRule() for rule in rules)
+    for rule in rules:
+        if not rule.isRangeRestricted():
+            raise ValueError("Rule Not Range Restricted", repr(rule))
     if reorderBodies:
       rules = list((Rule(head=rule.head, body=rule.body.reorder()) if isinstance(rule.body, Conjunction) else rule) for rule in rules)
     numberOfRules = len(rules)
     self.initial = []
     self.next = []
+    self.always = []
     unstratified = []
+    # Split Rules
     while rules:
       rule = rules.pop()
       if isinstance(rule.head, TempAnnotatedFormula):
         if rule.head.temporalAnnotation is start:
+          if rule.body is not None:
+            raise ValueError("Rule Body for start facts must be empty")
           self.initial.append(rule)
         elif rule.head.temporalAnnotation is next:
           self.next.append(rule)
@@ -195,16 +272,38 @@ class Program(object):
           raise ValueError("Unknown Temporal Annotation", rule.head.temporalAnnotation)
       elif isinstance(rule.head, Formula):
         if rule.body is None:
-          self.initial.append(rule)
+          self.always.append(rule)
         elif isinstance(rule.body, OracleFormula) or isinstance(rule.body, NegatedOracleFormula):
-          self.initial.append(rule)
+          self.always.append(rule)
+        elif isinstance(rule.body, Conjunction) and all(isinstance(formula, OracleFormula) or isinstance(formula, NegatedOracleFormula) for formula in rule.body.asConjunction()):
+            self.always.append(rule)
         else:
           unstratified.append(rule)
       elif isinstance(rule.head, CallFormula):
         self.next.append(rule)
       else:
         raise ValueError("Unsupported rule head", rule.head)
-    assert numberOfRules == len(self.initial) + len(self.next) + len(unstratified)
+    assert numberOfRules == len(self.initial) + len(self.next) + len(self.always) + len(unstratified)
+    # Create dependency graph
+    deps = set()
+    def make_edge(head, body):
+        if isinstance(body, Formula) and head.fn != body.fn:
+            deps.add((head.fn, 0, body.fn))
+        elif isinstance(body, NegatedFormula):
+            deps.add((head.fn, -1, body.orig.fn))
+        else:
+            pass
+    print(unstratified)
+    for rule in unstratified:
+        assert rule.body is not None
+        if isinstance(rule.body, Formula) or isinstance(rule.body, NegatedFormula):
+            make_edge(rule.head, rule.body)
+        elif isinstance(rule.body, Conjunction):
+            for lit in rule.body.asConjunction():
+                make_edge(rule.head, lit)
+        else:
+            raise ValueError("Unsupported Rule configuration", rule)
+    print(deps)
 
   def run(self, cycles=None, cb=None, fnmapping=dict()):
     pass
