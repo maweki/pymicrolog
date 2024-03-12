b14057f3b91707c47d402946cad0d6c519a362d8
Mario Wenzel <maweki@gmail.com>
2024-03-12 10:14:54 +0100

    edge follower example niced up

--- example.ev3dev_edge_follower.py
+++ example.ev3dev_edge_follower.py
@@ -14,16 +14,16 @@ sensor_get_value = call((lambda i: i.reflected_light_intensity))
 tacho_go = call((lambda m, s: m.run_forever(speed_sp=s)))
 lightval = relation("lightval")
 onLine = relation("online")
-lt = oracle(operator.lt)
-L = variable("L")
+config = relation("config")
 
-console_out = call(print)
+L = variable("L")
+K, V = variables(*"KV")
 
 rules = [
+    config()
     sensor_get_value(i1)@NEXT,
     lightval(L) <= sensor_get_value(i1, L),
-    onLine() <= lightval(L) & lt(L, 20), # can be written as (L < 20)
-    console_out(L)@NEXT <= lightval(L),
+    onLine() <= lightval(L) & (L < 20),
 
     tacho_go(m1, 50)@NEXT <= onLine(),
     tacho_go(m2, 200)@NEXT <= onLine(),
